f3d9a3d949aefb4be73ffe36d982fce6bd2835ef
kkk <kkk@kkk.com>
2018-03-15 20:58:30 +0800

    update

--- www/wechat.zhbx.com/app/Http/Controllers/Web/WechatController.php
+++ www/wechat.zhbx.com/app/Http/Controllers/Web/WechatController.php
@@ -33,9 +33,24 @@ class WechatController extends WebBaseController{
         $code = $request->get('code');
         
         $wechat_user_data = $wechat_oauth_business->getWeixinUserInfo($code);
+    
+        
+        $wx_user_info = array();
+        $wx_user_info['openid'] = $wechat_user_data['id'];
+        $wx_user_info['nickname'] = $wechat_user_data['nickname'];
+        $wx_user_info['avatar'] = $wechat_user_data['avatar'];
+        $wx_user_info['email'] = $wechat_user_data['email'];
+        if(!empty($wechat_user_data['original'])) {
+            $wx_user_info['sex'] = $wechat_user_data['original']['sex'];
+            $wx_user_info['language'] = $wechat_user_data['original']['language'];
+            $wx_user_info['city'] = $wechat_user_data['original']['city'];
+            $wx_user_info['province'] = $wechat_user_data['original']['province'];
+            $wx_user_info['country'] = $wechat_user_data['original']['country'];
+            $wx_user_info['headimgurl'] = $wechat_user_data['original']['headimgurl'];
+        }
         
         //同步用户数据
-        dd($wechat_user_data);
+        dd($wx_user_info);
         
         return redirect($target_url);
     }

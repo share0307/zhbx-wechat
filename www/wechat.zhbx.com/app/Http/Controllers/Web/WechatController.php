438b38c0a2e0d4e02378a845885cfb6c0291885a
kkk <kkk@kkk.com>
2018-03-16 11:47:35 +0800

    update

--- www/wechat.zhbx.com/app/Http/Controllers/Web/WechatController.php
+++ www/wechat.zhbx.com/app/Http/Controllers/Web/WechatController.php
@@ -56,22 +56,24 @@ class WechatController extends WebBaseController{
             }
         }
         
-        $wx_user_info = array();
-        $wx_user_info['openid'] = $wechat_user_data['id'];
-        $wx_user_info['nickname'] = $wechat_user_data['nickname'];
-        $wx_user_info['avatar'] = $wechat_user_data['avatar'];
-        $wx_user_info['email'] = $wechat_user_data['email'];
-        if(!empty($wechat_user_data['original'])) {
-            $wx_user_info['sex'] = $wechat_user_data['original']['sex'];
-            $wx_user_info['language'] = $wechat_user_data['original']['language'];
-            $wx_user_info['city'] = $wechat_user_data['original']['city'];
-            $wx_user_info['province'] = $wechat_user_data['original']['province'];
-            $wx_user_info['country'] = $wechat_user_data['original']['country'];
-            $wx_user_info['headimgurl'] = $wechat_user_data['original']['headimgurl'];
-        }
+        dd($wechat_user_data);
+        
+//        $wx_user_info = array();
+//        $wx_user_info['openid'] = $wechat_user_data['id'];
+//        $wx_user_info['nickname'] = $wechat_user_data['nickname'];
+//        $wx_user_info['avatar'] = $wechat_user_data['avatar'];
+//        $wx_user_info['email'] = $wechat_user_data['email'];
+//        if(!empty($wechat_user_data['original'])) {
+//            $wx_user_info['sex'] = $wechat_user_data['original']['sex'];
+//            $wx_user_info['language'] = $wechat_user_data['original']['language'];
+//            $wx_user_info['city'] = $wechat_user_data['original']['city'];
+//            $wx_user_info['province'] = $wechat_user_data['original']['province'];
+//            $wx_user_info['country'] = $wechat_user_data['original']['country'];
+//            $wx_user_info['headimgurl'] = $wechat_user_data['original']['headimgurl'];
+//        }
         
         //同步用户数据
-        dd($wx_user_info);
+//        dd($wx_user_info);
         
         redirect:
         //把用户信息写入 session，然后跳转
